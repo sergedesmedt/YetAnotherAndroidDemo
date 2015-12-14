4227706d512137d863523dbd1a9bba0f2d289526
Serge Desmedt <sergedesmedt@yahoo.com>
2015-12-14 21:27:48 +0100

    more property animation: x position

--- YetAnotherAnimationDemo/src/main/java/com/example/sergedesmedt/yaad/SimplePropertyAnimationActivity.java
+++ YetAnotherAnimationDemo/src/main/java/com/example/sergedesmedt/yaad/SimplePropertyAnimationActivity.java
@@ -1,11 +1,16 @@
 package com.example.sergedesmedt.yaad;
 
+import android.animation.AnimatorInflater;
+import android.animation.AnimatorSet;
+import android.animation.ObjectAnimator;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
 import android.widget.ImageView;
 
 import com.example.sergedesmedt.yaad.R;
@@ -19,6 +24,14 @@ public class SimplePropertyAnimationActivity extends AppCompatActivity {
         setContentView(R.layout.activity_simple_property_animation);
 
         imageView = (ImageView) findViewById(R.id.imageView1);
+    }
+
+    public void startPosXAnimation(View arg0) {
+        //ObjectAnimator.ofFloat(imageView, "x", 100f).setDuration(4000).start();
+        ObjectAnimator set = (ObjectAnimator) AnimatorInflater.loadAnimator(SimplePropertyAnimationActivity.this,
+                R.anim.simple_property_posx);
+        set.setTarget(imageView);
+        set.start();
     }
 
 }
